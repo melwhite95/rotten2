d79933a9c8bd61ee1699b8db83ebd4e10a39222e
melwhite95 <melwhite3895@gmail.com>
2018-10-24 15:09:27 -0700

    users can see single reviews

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const bodyParser = require('body-parser');
 
 
 mongoose.connect('mongodb://localhost/rotten-potatoes');
-const Review = mongoose.model('Review', {
-  title: String,
-  movieTitle: String
-});
+  const Review = mongoose.model('Review', {
+    title: String,
+    description: String,
+    movieTitle: String
+  });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -36,13 +37,20 @@ app.get('/reviews/new', (req, res) => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
 })
-
 
 
 
